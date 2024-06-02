76df133e165a73e39cb937eac359adeef735a549
GillolaHemaVardhanReddy <gillolahemavardhanreddy@gmail.com>
2024-06-02 11:43:06 +0530

    adding client to git

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+// import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter,Routes,Route} from "react-router-dom";
 import ShopCategory from './pages/ShopCategory';
